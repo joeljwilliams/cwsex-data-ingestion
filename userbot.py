ef50a427351cc2229f912635d5aba41a57f57c86
Joel <joel@yoloswag.in>
2018-05-01 15:32:23 +0200

    Heroku deploy support

--- userbot.py
+++ userbot.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
-from getpass import getpass
 from os import environ
 
 from telethon import TelegramClient, events
-from telethon.errors import SessionPasswordNeededError
+from alchemysession import AlchemySessionContainer
 
 import logging
 
@@ -14,19 +13,28 @@ logger = logging.getLogger(__name__)
 
 proxy_chan_id = 1151984662
 
-
+container = AlchemySessionContainer(environ['DATABASE_URL'])
 session_name = environ.get('TG_SESSION', 'session')
+session = container.new_session(session_name)
+
 user_phone = environ['TG_PHONE']
 client = TelegramClient(
-    session_name, int(environ['TG_API_ID']), environ['TG_API_HASH'],
+    session, int(environ['TG_API_ID']), environ['TG_API_HASH'],
     proxy=None, update_workers=4, spawn_read_thread=False
 )
 
-client.start()
+
+def code_cb():
+    return environ['TG_CODE']
+
+
+client.start(phone=user_phone, code_callback=code_cb)
+
 
 @client.on(events.NewMessage(incoming=True, chats='cwsex'))
 def message_handler(event):
     print(event)
     event.forward_to(proxy_chan_id)
+
 
 client.idle()
